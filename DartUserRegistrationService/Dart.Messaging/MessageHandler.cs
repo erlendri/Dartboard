4d4c9fede20f6757bbd9b63549df989006a1eb81
Erlend Richardsen <eri@Teleplan.no>
2019-05-15 19:16:47 +0200

    Added Messaging client and test console project.

---
+++ DartUserRegistrationService/Dart.Messaging/MessageHandler.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using static uPLibrary.Networking.M2Mqtt.MqttClient;
+
+namespace Dart.Messaging
+{
+    public interface IMessageHandler
+    {
+        void Subscribe(string topic);
+        void Publish<T>(string topic, T payload);
+    }
+
+    public class MqttMessageHandler : IMessageHandler
+    {
+        private MqttClient myClient;
+
+        public MqttMessageHandler(string uri)
+        {
+            myClient = new MqttClient(uri);
+
+            string clientId = Guid.NewGuid().ToString();
+            myClient.Connect(clientId);
+        }
+
+
+        public event MqttMsgPublishEventHandler MqttMsgPublishReceived
+        {
+            add
+            {   
+                    myClient.MqttMsgPublishReceived += value;   
+            }
+            remove
+            {
+                myClient.MqttMsgPublishReceived -= value;
+            }
+        }
+
+        public int MyProperty { get; set; }
+
+        public void Publish<T>(string topic, T payload)
+        {
+            var serializedPayload = JsonConvert.SerializeObject(payload);
+            myClient.Publish(topic, Encoding.UTF8.GetBytes(serializedPayload), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+        }
+
+        public void Subscribe(string topic)
+        {
+            myClient.Subscribe(new string[] { topic }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+        }
+
+        public void Disconnect()
+        {
+            myClient.Disconnect();
+        }
+    }
+}
---
+++ DartUserRegistrationService/ScoreboardTestBed/Program.cs
@@ -0,0 +1,49 @@
+﻿using DartUserRegistrationService.Models;
+using System;
+using System.Net;
+using System.Text;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using Newtonsoft.Json;
+using Dart.Messaging;
+
+namespace ScoreboardTestBed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new MqttMessageHandler("127.0.0.1");
+
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+
+            client.Subscribe("/geopackman/score");
+
+            Gamer packmanPlayer = new Gamer()
+            {
+                Email ="email@gamer.com",
+                FirstName="John",
+                LastName="Doe"
+            };
+
+
+            client.Publish<Gamer>("/geopackman/score", packmanPlayer);
+         
+            
+        }
+
+        
+
+        private static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            var message = Encoding.Default.GetString (e.Message);
+            Console.WriteLine($"Score received: {message}");
+
+            var wiredGamer = JsonConvert.DeserializeObject<Gamer>(message);
+           
+            Console.WriteLine($"Email from transferred gamer: {wiredGamer.Email}");
+            Console.ReadKey();
+        }
+    }
+}
