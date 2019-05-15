7009e31b8935c154e7d85fc5a2b9950512957a42
borret <borret@gmail.com>
2019-05-15 17:26:16 +0200

    Added qr/registration project

---
+++ DartUserRegistrationService/NDCRegistration/Models/Gamer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NDCRegistration.Models
+{
+    public class Gamer
+    {
+        public Guid Id { get; set; }
+        [DisplayName("Display name")]
+        [Required]
+        public string DisplayName { get; set; }
+        [DisplayName("First name")]
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        [DisplayName("Last name")]
+        public string LastName { get; set; }
+        [Required]
+        [DisplayName("Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DisplayName("QrCode")]
+        public string QrCode { get; set; }
+
+    }
+}
