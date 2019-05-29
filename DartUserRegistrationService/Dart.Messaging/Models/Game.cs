32f3c23f6f852025cdbfb84a886736ba95f200e0
borret <borret@gmail.com>
2019-05-29 17:59:53 +0200

    Added interface for gamer storage ++

---
+++ DartUserRegistrationService/Dart.Messaging/Models/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dart.Messaging.Models
+{
+    public enum GameState
+    {
+        Pending,
+        Completed,
+        Deleted
+    }
+    public class Game
+    {
+        public Guid GamerId { get; set; }
+        public Guid Id { get; set; }
+
+        public int Score { get; set; }
+        public GameState State { get; set; }
+
+    }
+}
--- DartUserRegistrationService/Dart.Messaging/Topics.cs
+++ DartUserRegistrationService/Dart.Messaging/Topics.cs
@@ -9,5 +9,16 @@ namespace Dart.Messaging
         public const string Gamer = "/geopackman/Gamer";
         public const string Score = "/geopackman/Score";
 
+        ///// <summary>
+        /// "{\"id\": \"007\", \"name\": \"James Bond\", \"maxTries\":3}"
+        /// </summary>
+        public const string GameStart = "hackathon/player";
+
+        //{"id":"007","name":"James Bond","maxTries":3,"score":16}
+        public const string ScoreUpdate = "hackathon/player/score";
+
+        public const string GameCompleted = "hackathon/player/completed";
+        public const string GameAborted = "hackathon/player/aborted";
+
     }
 }
